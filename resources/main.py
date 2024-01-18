0567e09f0c457f4b3e1db51ec3d2c09c46b9b4bc
czarekemce <c.szymanski@interia.eu>
2024-01-18 16:28:14 +0100

    Test GHA

--- resources/main.py
+++ resources/main.py
@@ -63,7 +63,7 @@ new_user_info = create_service_account()
 user_policy_arn = f"{new_user_info['UserArn']}"
 
 def create_s3_read_policy():
-    
+
     iam_client = aws_session.resource("iam")
 
     path='/'
